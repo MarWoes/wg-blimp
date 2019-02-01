778fc5c4c25a14e0285e589f98575fd0434d6740
Marius Wöste <mar.w@wwu.de>
2019-02-01 16:42:26 +0100

    Add config generation to wrapper

--- cli/cli.py
+++ cli/cli.py
@@ -1,4 +1,5 @@
 import click
+import cli.config
 
 @click.group()
 def main():
@@ -8,3 +9,6 @@ def main():
 def create_config():
 
     click.echo('welcome to wg-blimp')
+    test_config = cli.config.get_default_config('/scripts/test/fastq', '/scripts/test/lambda-phage.fa', '/output', ['simulated1', 'simulated2'], ['simulated3', 'simulated4'], 'hg38')
+
+    cli.config.dump_config(test_config, 'test-config.yaml')
---
+++ cli/config.py
@@ -0,0 +1,59 @@
+import yaml
+
+CGI_LOCATION_FILE    = 'annotation/cgi-locations-{}.csv.gz'
+GENE_ANNOTATION_FILE = 'annotation/gene-locations-{}.csv.gz'
+REPEAT_MASKER_FILE   = 'annotation/repeat-masker-{}.csv.gz'
+TSS_FILE             = 'annotation/transcription-start-sites-{}.csv.gz'
+
+DEFAULT_OPTIONALS_FILE = 'cli/optionals.yaml'
+
+def get_reference_annotation_files(reference_genome):
+
+    return {
+        'cgi_annotation_file':
+          CGI_LOCATION_FILE.format(reference_genome),
+
+        'gene_annotation_file':
+          GENE_ANNOTATION_FILE.format(reference_genome),
+
+        'repeat_masker_annotation_file':
+          REPEAT_MASKER_FILE.format(reference_genome),
+
+        'transcript_start_site_file':
+          TSS_FILE.format(reference_genome)
+    }
+
+def get_default_optional_parameters():
+
+    with open(DEFAULT_OPTIONALS_FILE) as f:
+
+        default_optionals = yaml.load(f)
+
+        return default_optionals
+
+def get_default_config(fastq_dir, fasta_ref, output_dir, group1, group2, reference_genome):
+
+    mandatory_parameters = {
+        'rawdir': fastq_dir,
+        'output_dir': output_dir,
+        'group1': group1,
+        'group2': group2,
+        'samples': group1 + group2,
+        'ref': fasta_ref
+    }
+
+    optional_parameters = get_default_optional_parameters()
+
+    reference_annotation_files = get_reference_annotation_files(reference_genome)
+
+    return {
+        **mandatory_parameters,
+        **optional_parameters,
+        **reference_annotation_files
+    }
+
+def dump_config(config_dict, target_file):
+
+    with (open(target_file, 'w')) as f:
+
+        yaml.dump(config_dict, f, default_flow_style=False)
