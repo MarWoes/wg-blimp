b847e295ae4bc770b65005ffee2a42b8261d35ff
Marius Wöste <mar.w@wwu.de>
2019-02-06 17:39:28 +0100

    Add own subcommand to delete all output
    This way, it's much harder to accidentally delete everything.

--- cli/snakemake.py
+++ cli/snakemake.py
@@ -2,13 +2,12 @@ import os
 import snakemake
 import cli.config
 
+# get right directory, see https://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
+script_dir = os.path.dirname(os.path.realpath(__file__))
+snakefile_location = os.path.join(script_dir,'..','snakemake', 'Snakefile')
 
-def run_snakemake_from_config(dry_run, delete_all_output, config_yaml):
 
-    # get right directory, see https://stackoverflow.com/questions/4934806/how-can-i-find-scripts-directory-with-python
-    script_dir = os.path.dirname(os.path.realpath(__file__))
-
-    snakefile_location = os.path.join(script_dir,'..','snakemake', 'Snakefile')
+def run_snakemake_from_config(dry_run, config_yaml, delete_all_output=False):
 
     config_dir= os.path.dirname(os.path.realpath(config_yaml))
 
@@ -22,7 +21,7 @@ def run_snakemake_from_config(dry_run, delete_all_output, config_yaml):
     )
 
 
-def run_snakemake(dry_run, delete_all_output, use_sample_files, genome_build, fastq_dir, reference_fasta, group1, group2, output_dir):
+def run_snakemake(dry_run, use_sample_files, genome_build, fastq_dir, reference_fasta, group1, group2, output_dir):
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -31,4 +30,9 @@ def run_snakemake(dry_run, delete_all_output, use_sample_files, genome_build, fa
 
     cli.config.create_config(use_sample_files, genome_build, fastq_dir, reference_fasta, group1, group2, output_dir, yaml_config_file)
 
-    run_snakemake_from_config(dry_run, delete_all_output, yaml_config_file)
+    run_snakemake_from_config(dry_run, yaml_config_file)
+
+
+def delete_all_output(dry_run, config_yaml):
+
+    run_snakemake_from_config(dry_run, config_yaml, True)
