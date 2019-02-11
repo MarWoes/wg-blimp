c3ccf54ba8f0f30fc1cfdfb8151b802005e8943d
Marius Wöste <mar.w@wwu.de>
2019-02-11 17:41:43 +0100

    Add shiny cli command

---
+++ cli/shiny.py
@@ -0,0 +1,41 @@
+import subprocess
+import tempfile
+import shutil
+import os
+
+
+def dump_dirs_to_file(project_dirs, target_file):
+
+    with (open(target_file, 'w')) as f:
+
+        f.write('{}\n'.format(project_dirs))
+
+
+def get_shiny_starting_command_string(host, port):
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    shiny_code_dir = os.path.join(script_dir,'..','shiny')
+
+    return 'shiny::runApp(appDir = "{}", host = "{}", port = {})'.format(shiny_code_dir, host, port)
+
+def start_shiny(project_dirs, host, port):
+
+    shiny_workdir = tempfile.mkdtemp()
+
+    projects_file = os.path.join(shiny_workdir, 'projects.txt')
+    multiqc_dir = os.path.join(shiny_workdir, 'multiqc')
+
+    dump_dirs_to_file(project_dirs, projects_file)
+
+    print(shiny_workdir)
+
+    shiny_running_command = [
+        'Rscript',
+        '-e', 'projectsFileArgument <- "{}"'.format(projects_file),
+        '-e', 'multiqcDirArgument <- "{}"'.format(multiqc_dir),
+        '-e', get_shiny_starting_command_string(host, port)
+    ]
+
+    subprocess.run(shiny_running_command)
+
+    shutil.rmtree(shiny_workdir)
