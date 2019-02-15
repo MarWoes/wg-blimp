dd73c6ed1cea02830924790bbf7bd42b71a766c3
Marius Wöste <mar.w@wwu.de>
2019-02-15 11:34:23 +0100

    Push version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='wg-blimp',
-    version='0.9.0',
+    version='0.9.1',
     author='Marius Woeste',
     author_email='mar.w@wwu.de',
     description='WGBS methylation analysis pipeline',
