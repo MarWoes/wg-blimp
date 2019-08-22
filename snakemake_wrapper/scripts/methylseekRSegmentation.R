d68b31b8485133691aace82d4dbfff1aea16c9a8
Marius Wöste <mar.w@wwu.de>
2019-08-22 17:56:03 +0200

    Add UMR/LMR computation

--- snakemake_wrapper/scripts/methylseekRSegmentation.R
+++ snakemake_wrapper/scripts/methylseekRSegmentation.R
@@ -4,6 +4,67 @@ library(stringr)
 library(data.table)
 library(rtracklayer)
 library(parallel)
+
+
+### FUNCTIONS
+
+segmentIntoUMRsAndLMRs <- function(methylationRanges, cgiRanges, numThreads, targetDir, genomeSeq, seqLengths, pmdSegments) {
+
+  fdrStats <- calculateFDRs(
+    m = methylationRanges,
+    CGIs = cgiRanges,
+    PMDs = pmdSegments,
+    num.cores = numThreads,
+    pdfFilename = paste0(targetDir, "/fdr.stats.pdf")
+  )
+
+  # TODO: use other values than the default one?
+  fdrCutoff <- 5
+  methylationCutoff <- 0.5
+  selectedN <- as.integer(names(fdrStats$FDRs[as.character(methylationCutoff), ][fdrStats$FDRs[as.character(methylationCutoff), ] < fdrCutoff])[1])
+
+  segments <- segmentUMRsLMRs(
+    m = methylationRanges,
+    meth.cutoff = methylationCutoff,
+    nCpG.cutoff = selectedN,
+    PMDs = pmdSegments,
+    num.cores = numThreads,
+    myGenomeSeq = genomeSeq,
+    seqLengths = seqLengths,
+    pdfFilename = paste0(targetDir, "/umr-lmr-heatmap.pdf")
+  )
+
+  plotFinalSegmentation(
+    m = methylationRanges,
+    segs = segments,
+    PMDs = pmdSegments,
+    meth.cutoff = methylationCutoff,
+    pdfFilename = paste0(targetDir, "/umr-lmr-segmentation.pdf")
+  )
+
+  saveUMRLMRSegments(
+    segments,
+    TableFilename = paste0(targetDir, "/umr-lmr.tab")
+  )
+}
+
+loadOrInstall <- function(packageName) {
+
+  isPackageInstalled <- packageName %in% rownames(installed.packages())
+
+  if (!isPackageInstalled) {
+
+    if (!requireNamespace("BiocManager", quietly = TRUE))
+      install.packages("BiocManager", repo = "http://cran.rstudio.com/")
+
+    BiocManager::install(packageName)
+  }
+
+  library(packageName, character.only = TRUE)
+}
+
+### INPUT
+
 # if (exists("snakemake")) {
 #   save.image("methylseekr-debug.rds")
 # }
@@ -12,78 +73,86 @@ samples <- snakemake@params$samples
 targetDir <- snakemake@params$target_dir
 methylationDir <- snakemake@params$methylation_dir
 calibrationChr <- snakemake@params$calibration_chr
-cgiTargetGenome <- snakemake@params$genome_build
-
-
+targetGenomeName <- snakemake@params$genome_build
 numThreads <- snakemake@threads
 
-sapply(paste0(targetDir, "/", samples), dir.create, showWarnings = FALSE)
+### SCRIPT
 
-referenceBS <- readDNAStringSet(fastaRef)
+targetGenomeBS <- paste0("BSgenome.Hsapiens.UCSC.", targetGenomeName)
 
-# according to FASTA defaults, only the ID before the first whitespace is considered. Also remove any 'chr's
-names(referenceBS) <- str_remove(names(referenceBS), "\\s.*")
-names(referenceBS) <- str_remove(names(referenceBS), "^chr")
-calibrationChr <- str_remove(calibrationChr, "^chr")
+loadOrInstall(targetGenomeBS)
+
+referenceBS <- get(targetGenomeBS)
+
+# since own FASTA files may not be used with methylseekr, UCSC/GRCh 'chr's need to be taken care of
+calibrationChr <- str_replace(calibrationChr, "^(?!chr)", "chr")
 
 # query CGI's
 session <- browserSession()
-genome(session) <- cgiTargetGenome
+genome(session) <- targetGenomeName
 query <- ucscTableQuery(session, "cpgIslandExt")
 cgiRanges <- track(query)
 genome(cgiRanges) <- NA
 cgiRanges <- suppressWarnings(resize(cgiRanges, 5000, fix = "center"))
-cgiRanges <- renameSeqlevels(cgiRanges, str_remove(as.character(seqlevels(cgiRanges)), "^chr"))
 
 for (sample in samples) {
-  
+
   methylationValues <- fread(
     paste0(methylationDir, "/", sample, "_CpG.bedGraph"),
     skip = 1L,
     header = FALSE,
     col.names = c("chr", "start", "end", "methPercent", "methylated", "unmethylated")
   )
-  
+
+  # adjust possibly faulty 'chr's here too
+  methylationValues$chr <- str_replace(methylationValues$chr, "^(?!chr)", "chr")
+
   sampleRanges <- GRanges(methylationValues$chr, ranges = methylationValues$start)
   values(sampleRanges)$T <- methylationValues$methylated + methylationValues$unmethylated
   values(sampleRanges)$M <- methylationValues$methylated
-  
+
   pmdSegments <- segmentPMDs(
     m = sampleRanges,
     chr.sel = calibrationChr,
     num.cores = numThreads,
     seqLengths = seqlengths(referenceBS),
-    pdfFilename = paste0(targetDir, "/", sample, "/alpha.", calibrationChr, ".pdf")
+    pdfFilename = paste0(targetDir, "/", sample, "/alphaCalibration.pdf")
   )
 
   plotAlphaDistributionOneChr(
     m = subsetByOverlaps(sampleRanges, pmdSegments[values(pmdSegments)$type == "notPMD"]),
     chr.sel = calibrationChr,
     num.cores = numThreads,
-    pdfFilename = paste0(targetDir, "/", sample, "/alpha.pmd.removed.", calibrationChr, ".pdf")
+    pdfFilename = paste0(targetDir, "/", sample, "/alphaPMDRemoved.pdf")
   )
-  
+
   plotPMDSegmentation(
     m = sampleRanges,
     segs = pmdSegments,
     pdfFilename =  paste0(targetDir, "/", sample, "/pmd.pdf")
   )
-  
+
   savePMDSegments(pmdSegments, TableFilename = paste0(targetDir, "/", sample, "/pmd-segments.tab"))
-  
-  fdrStats <- calculateFDRs(
-    m = sampleRanges,
-    CGIs = cgiRanges,
-    PMDs = pmdSegments,
-    num.cores = numThreads,
-    pdfFilename = paste0(targetDir, "/", sample, "/fdr.stats.pdf")
+
+  segmentIntoUMRsAndLMRs(
+    methylationRanges = sampleRanges,
+    cgiRanges = cgiRanges,
+    numThreads = numThreads,
+    targetDir = paste0(targetDir, "/", sample, "/LMRUMRwithPMD/"),
+    genomeSeq = referenceBS,
+    seqLengths = seqlengths(referenceBS),
+    pmdSegments = pmdSegments
   )
-  
-  # TODO: replace with non-default values?
-  fdrCutoff <- 5
-  selectedMethylation <- 0.5
-  selectedNumber <- as.integer(names(fdrStats$FDRs[as.character(selectedMethylation), ][fdrStats$FDRs[as.character(selectedMethylation), ] < fdrCutoff])[1])
-  
+
+  segmentIntoUMRsAndLMRs(
+    methylationRanges = sampleRanges,
+    cgiRanges = cgiRanges,
+    numThreads = numThreads,
+    targetDir = paste0(targetDir, "/", sample, "/LMRUMRwithoutPMD/"),
+    genomeSeq = referenceBS,
+    seqLengths = seqlengths(referenceBS),
+    pmdSegments = NA
+  )
 
 }
 
