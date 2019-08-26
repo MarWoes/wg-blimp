e409a3b3badf15302f89decbdc15a7b466e68ee8
Marius Wöste <mar.w@wwu.de>
2019-08-26 17:36:16 +0200

    Partially refactor dmr annotation into own module
    ... thus we can just re-use the existing stuff for PMD/UMR/LMR
    annotation

--- snakemake_wrapper/scripts/dmrAnnotation.R
+++ snakemake_wrapper/scripts/dmrAnnotation.R
@@ -3,89 +3,16 @@ if (exists("snakemake")) {
 
   sink(logFile, append = TRUE)
   sink(logFile, append = TRUE, type = "message")
+
+  snakemake@source("regionAnnotation.R")
 }
 
-library(data.table)
-library(GenomicRanges)
-library(stringr)
-
-wgbs.annotateOverlap <- function (dmrRanges, overlapRanges, geneTable) {
-
-  intersection <- as.list(findOverlaps(dmrRanges, overlapRanges))
-  isOverlapping <- sapply(intersection, length) > 0
-  overlappingGeneNames <- sapply(intersection, function (indices) paste(unique(geneTable$external_gene_name[indices]), collapse = ","))
-
-  return(data.table(
-    isOverlapping,
-    overlappingGeneNames
-  ))
-}
-
-wgbs.annotateGenes <- function (dmrs, dmrRanges, genes) {
-
-  geneRanges <- GRanges(seqnames = genes$chromosome_name, ranges = IRanges(genes$start_position, genes$end_position))
-
-  dmrs[,c("gene_overlap", "gene_name")] <- wgbs.annotateOverlap(dmrRanges, geneRanges, genes)
-
-  return(dmrs)
-}
-
-wgbs.annotateCGIslands <- function (dmrs, dmrRanges, cgis) {
-
-  cgiRanges  <- GRanges(seqnames = cgis$chrom, ranges = IRanges(cgis$chromStart, cgis$chromEnd))
-
-  dmrs$cgi_overlap <- countOverlaps(dmrRanges, cgiRanges) > 0
-
-  return(dmrs)
-}
-
-wgbs.annotatePromoters <- function (dmrs, dmrRanges, transcriptStartSites, promoterTSSDistances) {
-
-  promoterStarts <- transcriptStartSites$transcription_start_site + promoterTSSDistances[1]
-  promoterEnds <- transcriptStartSites$transcription_start_site + promoterTSSDistances[2]
-
-  promoterRanges <- GRanges(seqnames = transcriptStartSites$chromosome_name, ranges = IRanges(promoterStarts, promoterEnds))
-
-  dmrs[,c("promoter_overlap", "promoter_name")] <- wgbs.annotateOverlap(dmrRanges, promoterRanges, transcriptStartSites)
-
-  return(dmrs)
-}
-
-wgbs.annotateRepeats <- function (dmrs, dmrRanges, repeats, classes) {
-
-  relevantRepeats <- repeats[repClass %in% classes]
-
-  repeatRanges <- GRanges(relevantRepeats$genoName, IRanges(relevantRepeats$genoStart, relevantRepeats$genoEnd))
-
-  dmrs$num_repeats <- countOverlaps(dmrRanges, repeatRanges)
-
-  return(dmrs)
-}
 
 wgbs.annotateDMRs <- function (dmrFile, gzippedCgiFile, gzippedGeneFile, gzippedRepeatMaskerAnnotationFile, gzippedTranscriptionStartSiteFile, gzippedCoverageFiles, allowedBiotypes, promoterTSSDistances, annotatedDmrFile) {
 
   dmrs <- fread(dmrFile)
 
-  cgis <- fread(paste("zcat", gzippedCgiFile))
-  genes <- fread(paste("zcat", gzippedGeneFile))
-  transcriptStartSites <- fread(paste("zcat", gzippedTranscriptionStartSiteFile))
-  repeats <- fread(paste("zcat", gzippedRepeatMaskerAnnotationFile))
-
-  cgis$chrom <- substr(cgis$chrom, 4, nchar(cgis$chrom))
-  repeats$genoName <- substr(repeats$genoName, 4, nchar(repeats$genoName))
-
-  genes <- genes[gene_biotype %in% allowedBiotypes]
-  transcriptStartSites <- transcriptStartSites[gene_biotype %in% allowedBiotypes]
-
-  dmrs$length <- dmrs$end - dmrs$start
-
-  # remove any trailing 'chr's for range intersection
-  dmrRanges  <- GRanges(seqnames = str_remove(dmrs$chr, "^chr"), ranges = IRanges(dmrs$start, dmrs$end))
-
-  dmrs <- wgbs.annotateGenes(dmrs, dmrRanges, genes)
-  dmrs <- wgbs.annotateCGIslands(dmrs, dmrRanges, cgis)
-  dmrs <- wgbs.annotatePromoters(dmrs, dmrRanges, transcriptStartSites, promoterTSSDistances)
-  dmrs <- wgbs.annotateRepeats(dmrs, dmrRanges, repeats, c("LINE", "SINE", "LTR", "DNA"))
+  dmrs <- annotation.annotateRegions(dmrs, gzippedCgiFile, gzippedGeneFile, gzippedRepeatMaskerAnnotationFile, gzippedTranscriptionStartSiteFile, allowedBiotypes, promoterTSSDistances)
 
   coverages <- sapply(gzippedCoverageFiles, function (gzippedCoverageFile) fread(paste("zcat", gzippedCoverageFile))$V4)
 
---
+++ snakemake_wrapper/scripts/regionAnnotation.R
@@ -0,0 +1,82 @@
+library(data.table)
+library(GenomicRanges)
+library(stringr)
+
+annotation.annotateOverlap <- function (regionRanges, overlapRanges, geneTable) {
+
+  intersection <- as.list(findOverlaps(regionRanges, overlapRanges))
+  isOverlapping <- sapply(intersection, length) > 0
+  overlappingGeneNames <- sapply(intersection, function (indices) paste(unique(geneTable$external_gene_name[indices]), collapse = ","))
+
+  return(data.table(
+    isOverlapping,
+    overlappingGeneNames
+  ))
+}
+
+annotation.annotateGenes <- function (regions, regionRanges, genes) {
+
+  geneRanges <- GRanges(seqnames = genes$chromosome_name, ranges = IRanges(genes$start_position, genes$end_position))
+
+  regions[,c("gene_overlap", "gene_name")] <- annotation.annotateOverlap(regionRanges, geneRanges, genes)
+
+  return(regions)
+}
+
+annotation.annotateCGIslands <- function (regions, regionRanges, cgis) {
+
+  cgiRanges  <- GRanges(seqnames = cgis$chrom, ranges = IRanges(cgis$chromStart, cgis$chromEnd))
+
+  regions$cgi_overlap <- countOverlaps(regionRanges, cgiRanges) > 0
+
+  return(regions)
+}
+
+annotation.annotatePromoters <- function (regions, regionRanges, transcriptStartSites, promoterTSSDistances) {
+
+  promoterStarts <- transcriptStartSites$transcription_start_site + promoterTSSDistances[1]
+  promoterEnds <- transcriptStartSites$transcription_start_site + promoterTSSDistances[2]
+
+  promoterRanges <- GRanges(seqnames = transcriptStartSites$chromosome_name, ranges = IRanges(promoterStarts, promoterEnds))
+
+  regions[,c("promoter_overlap", "promoter_name")] <- annotation.annotateOverlap(regionRanges, promoterRanges, transcriptStartSites)
+
+  return(regions)
+}
+
+annotation.annotateRepeats <- function (regions, regionRanges, repeats, classes) {
+
+  relevantRepeats <- repeats[repClass %in% classes]
+
+  repeatRanges <- GRanges(relevantRepeats$genoName, IRanges(relevantRepeats$genoStart, relevantRepeats$genoEnd))
+
+  regions$num_repeats <- countOverlaps(regionRanges, repeatRanges)
+
+  return(regions)
+}
+
+annotation.annotateRegions <- function (regionTable, gzippedCgiFile, gzippedGeneFile, gzippedRepeatMaskerAnnotationFile, gzippedTranscriptionStartSiteFile, allowedBiotypes, promoterTSSDistances) {
+
+  cgis <- fread(paste("zcat", gzippedCgiFile))
+  genes <- fread(paste("zcat", gzippedGeneFile))
+  transcriptStartSites <- fread(paste("zcat", gzippedTranscriptionStartSiteFile))
+  repeats <- fread(paste("zcat", gzippedRepeatMaskerAnnotationFile))
+
+  cgis$chrom <- substr(cgis$chrom, 4, nchar(cgis$chrom))
+  repeats$genoName <- substr(repeats$genoName, 4, nchar(repeats$genoName))
+
+  genes <- genes[gene_biotype %in% allowedBiotypes]
+  transcriptStartSites <- transcriptStartSites[gene_biotype %in% allowedBiotypes]
+
+  regionTable$length <- regionTable$end - regionTable$start
+
+  # remove any trailing 'chr's for range intersection
+  regionRanges  <- GRanges(seqnames = str_remove(regionTable$chr, "^chr"), ranges = IRanges(regionTable$start, regionTable$end))
+
+  regionTable <- annotation.annotateGenes(regionTable, regionRanges, genes)
+  regionTable <- annotation.annotateCGIslands(regionTable, regionRanges, cgis)
+  regionTable <- annotation.annotatePromoters(regionTable, regionRanges, transcriptStartSites, promoterTSSDistances)
+  regionTable <- annotation.annotateRepeats(regionTable, regionRanges, repeats, c("LINE", "SINE", "LTR", "DNA"))
+
+  return(regionTable)
+}
