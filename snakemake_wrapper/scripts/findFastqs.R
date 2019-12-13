52c1f828aadae5ac560fbc90920b3c0bf7168db5
Marius Wöste <mar.w@wwu.de>
2019-12-13 13:40:59 +0100

    Add optional sample <-> fastq CSV file

--- snakemake_wrapper/scripts/findFastqs.R
+++ snakemake_wrapper/scripts/findFastqs.R
@@ -5,44 +5,93 @@ if (exists("snakemake")) {
   sink(logFile, append = TRUE, type = "message")
 }
 
+library(data.table)
+
+### FUNCTIONS
+
+guessFastqFiles <- function (rawDir, forwardReadRegex, reverseReadRegex, targetSample, allSamples) {
+
+  availableForwardReads <- list.files(rawDir, pattern = forwardReadRegex, full.names = TRUE, recursive = TRUE)
+  availableReverseReads <- list.files(rawDir, pattern = reverseReadRegex, full.names = TRUE, recursive = TRUE)
+
+  # prevent overlapping names being assigned wrong, e.g. 'sample11_1.fq.gz' being assigned to 'sample1'
+  superStringSamples <- allSamples[allSamples != targetSample & grepl(targetSample, allSamples, fixed = TRUE)]
+
+  for (superStringSample in superStringSamples) {
+
+    availableForwardReads <- availableForwardReads[!grepl(superStringSample, basename(availableForwardReads), fixed = TRUE)]
+    availableReverseReads <- availableReverseReads[!grepl(superStringSample, basename(availableReverseReads), fixed = TRUE)]
+
+  }
+
+  forwardReadMatches <- grepl(targetSample, basename(availableForwardReads), fixed = TRUE)
+  reverseReadMatches <- grepl(targetSample, basename(availableReverseReads), fixed = TRUE)
+
+
+  # sort read files to prevent any mismatching forward/reverse read situations
+  sortedForwardReads <- sort(availableForwardReads[forwardReadMatches])
+  sortedReverseReads <- sort(availableReverseReads[reverseReadMatches])
+
+  if (length(sortedForwardReads) == 0 || length(sortedReverseReads) == 0) {
+    stop(paste0("No forward or reverse reads found for sample ", targetSample, "."))
+  }
+
+  if (length(sortedForwardReads) != length(sortedReverseReads)) {
+    stop(paste0("Number of forward and reverse read files differs for sample ", targetSample, "."))
+  }
+
+  return(list(
+    forward = sortedForwardReads,
+    reverse = sortedReverseReads
+  ))
+}
+
+readFastqsFromCsv <- function (csvFile, targetSample) {
+
+  fastqMap <- fread(csvFile)
+
+  targetSampleReadFiles <- fastqMap[sample == targetSample]
+
+  forwardFiles <- targetSampleReadFiles$forward
+  reverseFiles <- targetSampleReadFiles$reverse
+
+  for (file in c(forwardFiles, reverseFiles)) {
+
+    if (!file.exists(file))
+      stop(paste0("Could not find file ", file))
+
+  }
+
+  return(list(
+    forward = forwardFiles,
+    reverse = reverseFiles
+  ))
+
+}
+
+### SCRIPT
+
 rawDir <- snakemake@params$rawdir
 allSamples <- snakemake@params$samples
 targetSample <- snakemake@wildcards$sample
-outputDir <- snakemake@params$output_dir
+
+forwardOutputFile <- snakemake@output$first
+reverseOutputFile <- snakemake@output$second
 
 forwardReadRegex <- snakemake@params$first_regex
 reverseReadRegex <- snakemake@params$second_regex
 
-availableForwardReads <- list.files(rawDir, pattern = forwardReadRegex, full.names = TRUE, recursive = TRUE)
-availableReverseReads <- list.files(rawDir, pattern = reverseReadRegex, full.names = TRUE, recursive = TRUE)
+sampleFastqCsv <- snakemake@params$sample_fastq_csv
 
-# prevent overlapping names being assigned wrong, e.g. 'sample11_1.fq.gz' being assigned to 'sample1'
-superStringSamples <- allSamples[allSamples != targetSample & grepl(targetSample, allSamples, fixed = TRUE)]
+if (length(sampleFastqCsv) == 1 && is.character(sampleFastqCsv) && file.exists(sampleFastqCsv)) {
 
-for (superStringSample in superStringSamples) {
+  sampleFastqFiles <- readFastqsFromCsv(sampleFastqCsv, targetSample)
 
-  availableForwardReads <- availableForwardReads[!grepl(superStringSample, basename(availableForwardReads), fixed = TRUE)]
-  availableReverseReads <- availableReverseReads[!grepl(superStringSample, basename(availableReverseReads), fixed = TRUE)]
+} else {
+
+  sampleFastqFiles <- guessFastqFiles(rawDir, forwardReadRegex, reverseReadRegex, targetSample, allSamples)
 
 }
 
-forwardReadMatches <- grepl(targetSample, basename(availableForwardReads), fixed = TRUE)
-reverseReadMatches <- grepl(targetSample, basename(availableReverseReads), fixed = TRUE)
-
-firstFastqFile <- paste0(outputDir, "/raw/", targetSample, ".first.txt")
-secondFastqFile <- paste0(outputDir, "/raw/", targetSample, ".second.txt")
-
-# sort read files to prevent any mismatching forward/reverse read situations
-sortedForwardReads <- sort(availableForwardReads[forwardReadMatches])
-sortedReverseReads <- sort(availableReverseReads[reverseReadMatches])
-
-if (length(sortedForwardReads) == 0 || length(sortedReverseReads) == 0) {
-  stop(paste0("No forward or reverse reads found for sample ", targetSample, "."))
-}
-
-if (length(sortedForwardReads) != length(sortedReverseReads)) {
-  stop(paste0("Number of forward and reverse read files differs for sample ", targetSample, "."))
-}
-
-writeLines(sortedForwardReads, firstFastqFile)
-writeLines(sortedReverseReads, secondFastqFile)
+writeLines(sampleFastqFiles$forward, forwardOutputFile)
+writeLines(sampleFastqFiles$reverse, reverseOutputFile)
