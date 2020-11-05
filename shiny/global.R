153054e6a113ca8f9f625b0d607339a8362e9f09
Marius Wöste <mar.w@wwu.de>
2020-11-05 16:33:13 +0100

    Remove R shiny auto-install
    ... dependencies will be installed via conda

--- shiny/global.R
+++ shiny/global.R
@@ -1,43 +1,12 @@
 options(repos = c(CRAN = "https://cran.rstudio.com"))
 
-shiny.wgbs.loadOrInstall <- function (packageName, type = "CRAN") {
-
-  isPackageInstalled <- packageName %in% rownames(installed.packages())
-
-  if (!isPackageInstalled) {
-
-    if (type == "CRAN") {
-
-      install.packages(packageName)
-
-    } else if (type == "bioc") {
-
-      if (!requireNamespace("BiocManager", quietly = TRUE))
-        install.packages("BiocManager")
-      BiocManager::install(packageName, version = "3.8")
-
-    }
-
-  }
-
-  library(packageName, character.only = TRUE)
-}
-
-cranPackages <- c(
-  "shiny",
-  "shinydashboard",
-  "data.table",
-  "ggplot2",
-  "htmlwidgets",
-  "DT",
-  "httpuv"
-)
-
-for (package in cranPackages) {
-
-  shiny.wgbs.loadOrInstall(package)
-
-}
+library(shiny)
+library(shinydashboard)
+library(data.table)
+library(ggplot2)
+library(htmlwidgets)
+library(DT)
+library(httpuv)
 
 source("datasets.R")
 source("multiqc.R")
