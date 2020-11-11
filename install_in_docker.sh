a571e143c1bceefaac779544027439134093bd29
Marius Wöste <mar.w@wwu.de>
2020-11-11 17:00:46 +0100

    Reduce number of cpus used for tests to 4

--- install_in_docker.sh
+++ install_in_docker.sh
@@ -18,16 +18,17 @@ unzip download
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
-conda install --yes --name base 'snakemake-minimal>=5.8' click ruamel.yaml r-base r-shiny r-shinydashboard r-data.table r-ggplot2 r-htmlwidgets r-dt r-httpuv h5py pysam
+conda install --yes --name base wg-blimp
+#conda install --yes --name base 'snakemake-minimal>=5.8' click ruamel.yaml r-base r-shiny r-shinydashboard r-data.table r-ggplot2 r-htmlwidgets r-dt r-httpuv h5py pysam
 conda clean --all --yes
 
-git clone --recursive https://github.com/MarWoes/wg-blimp /root/wg-blimp
-pip install /root/wg-blimp
+#git clone --recursive https://github.com/MarWoes/wg-blimp /root/wg-blimp
+#pip install /root/wg-blimp
 
 cd $TEST_DIR
 wg-blimp create-config --cores-per-job 4 fastq chr22.fasta blood1,blood2 sperm1,sperm2 results config.yaml
-wg-blimp run-snakemake-from-config --cores=16 --dry-run config.yaml
-wg-blimp run-snakemake-from-config --cores=16 config.yaml
+wg-blimp run-snakemake-from-config --cores=4 --dry-run config.yaml
+wg-blimp run-snakemake-from-config --cores=4 config.yaml
 
 cd /
 rm -rf $TEST_DIR
