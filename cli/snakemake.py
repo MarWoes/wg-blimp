72aa8042fc3ef5d6a624c6383cc1b15f90015d61
Marius Wöste <mar.w@wwu.de>
2021-04-07 12:17:47 +0200

    Add cluster parameters to snakemake cli

--- cli/snakemake.py
+++ cli/snakemake.py
@@ -7,7 +7,7 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 snakefile_location = os.path.join(script_dir,'..','snakemake_wrapper', 'Snakefile')
 conda_prefix = os.path.abspath(os.path.join(script_dir, '..', 'snakemake_wrapper', 'conda'))
 
-def run_snakemake_from_config(dry_run, config_yaml, cores, delete_all_output=False):
+def run_snakemake_from_config(dry_run, config_yaml, cores, cluster_command, nodes, delete_all_output=False):
 
     print("[INFO] Invoking Snakemake with config {} and {} cores.".format(config_yaml, cores))
 
@@ -16,17 +16,20 @@ def run_snakemake_from_config(dry_run, config_yaml, cores, delete_all_output=Fal
         configfiles=[config_yaml],
         dryrun=dry_run,
         cores=cores,
+        local_cores=cores,
+        nodes=nodes,
         printshellcmds=True,
         delete_all_output=delete_all_output,
         use_conda=True,
-        conda_prefix=conda_prefix
+        conda_prefix=conda_prefix,
+        cluster=cluster_command
     )
 
     if not finished_successfully:
         os.sys.exit(os.EX_SOFTWARE)
 
 
-def run_snakemake(dry_run, use_sample_files, cores, genome_build, fastq_dir, reference_fasta, group1, group2, output_dir):
+def run_snakemake(dry_run, use_sample_files, cores, genome_build, cluster_command, nodes, fastq_dir, reference_fasta, group1, group2, output_dir):
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
@@ -35,9 +38,9 @@ def run_snakemake(dry_run, use_sample_files, cores, genome_build, fastq_dir, ref
 
     cli.config.create_config(use_sample_files, genome_build, cores, fastq_dir, reference_fasta, group1, group2, output_dir, yaml_config_file)
 
-    run_snakemake_from_config(dry_run, yaml_config_file, cores)
+    run_snakemake_from_config(dry_run, yaml_config_file, cores, cluster_command, nodes)
 
 
 def delete_all_output(dry_run, config_yaml):
 
-    run_snakemake_from_config(dry_run, config_yaml, 1, True)
+    run_snakemake_from_config(dry_run, config_yaml, 1, None, 1, True)
