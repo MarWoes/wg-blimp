b63eaba2c7dd077a0c4f7cd7c130896f1efb5f9f
Marius Woeste <mar.w@wwu.de>
2022-07-27 23:35:09 +0200

    Make gemBS.sh portable on different *nix systems

--- snakemake_wrapper/scripts/gemBS.sh
+++ snakemake_wrapper/scripts/gemBS.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 cd $alignDir
 gemBS prepare -c gemBS.conf -t gemBS_cleaned.csv -o gemBS.json
 gemBS index
 gemBS map
-for file in *.bam; do 
-	mv "$file" "$(echo "$file" | sed s/bam/unmarked.bam/)"; 
+for file in *.bam; do
+	mv "$file" "$(echo "$file" | sed s/bam/unmarked.bam/)";
 done
 rm *.csi *.err *.md5
 exit
